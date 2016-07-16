995b1810b7fec43f815e0b9ba4cb960b7ac0d36d
charly132gx <carlosalfred132@gmail.com>
2016-07-16 09:29:39 -0500

    Signed-off-by: charly132gx <carlosalfred132@gmail.com>

---
+++ conexion.php
@@ -0,0 +1,20 @@
+<?php
+
+/*$usario="u691494598_xaar";
+$passwd="sanolmi132b";
+$servidor="mysql.hostinger.mx";
+$BD="u691494598_pc";*/
+
+$usario="root";
+$passwd="";
+$servidor="localhost";
+$BD="proyec";
+
+/*$usario="n260m_14426545";
+$passwd="drag0n";
+$servidor="sql206.260mb.net";
+$BD="n260m_14426545_pcatual";*/
+
+$conexion=mysqli_connect($servidor,$usario,$passwd,$BD)or die("Error al conectarse a la base de datos:".mysqli_error());
+//mysqli_select_db($BD,$conexion);
+?>
